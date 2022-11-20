03a297e3391c1165290d2cbeb272e2486ea21424
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2022-11-20 18:14:19 +0100

    baked scene

--- src/PostOffice.tsx
+++ src/PostOffice.tsx
@@ -1,7 +1,25 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
+import { useEffect } from "react";
+import { Mesh, MeshBasicMaterial, sRGBEncoding, Texture } from "three";
 
 export function PostOffice() {
   const postOfficeGltf = useGLTF("./post office.glb");
+  const bakedTexture = useTexture("./baked.jpg", (texture) => {
+    (texture as Texture).flipY = false;
+    (texture as Texture).encoding = sRGBEncoding;
+  });
+
+  useEffect(() => {
+    if (!postOfficeGltf?.scene || !bakedTexture) return;
+
+    const material = new MeshBasicMaterial({ map: bakedTexture });
+
+    postOfficeGltf.scene.traverse((child) => {
+      if (child instanceof Mesh) {
+        child.material = material;
+      }
+    });
+  }, [postOfficeGltf, bakedTexture]);
 
   // const { x, y, z } = useControls("Post office", {
   //   x: { value: -0.4, min: -5, max: 5 },
